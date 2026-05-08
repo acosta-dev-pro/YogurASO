0fcd8033055cca34de9552f1011736c02d815612
Acosta <acosta0095d@gmail.com>
2026-05-08 08:36:37 -0500

    productRoutes: product endpoints

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,8 +1,12 @@
 ﻿const express = require('express');
 const router = express.Router();
+const { getProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+const { verificarToken, verificarAdmin } = require('../middleware/verifyToken');
 
-router.get('/', (req, res) => {
-    res.json({ products: [] });
-});
+router.get('/', getProducts);
+router.get('/:id', getProductById);
+router.post('/', verificarToken, verificarAdmin, createProduct);
+router.put('/:id', verificarToken, verificarAdmin, updateProduct);
+router.delete('/:id', verificarToken, verificarAdmin, deleteProduct);
 
 module.exports = router;
