c8e6beb62221473659bff4a85837523bcff4c9f3
Acosta <acosta0095d@gmail.com>
2026-06-01 16:19:11 -0500

    Subir proyecto completo al repositorio, incluir backend, frontend, uploads, backup y nuevas rutas

---
+++ backend/src/controllers/testController.js
@@ -0,0 +1,12 @@
+﻿const getTest = (req, res) => {
+    console.log('✅ TEST CONTROLLER FUNCIONA');
+    res.json({ 
+        mensaje: 'SI FUNCIONA CORRECTAMENTE',
+        productos: [
+            { id: 1, nombre: 'Yogur de Prueba 1' },
+            { id: 2, nombre: 'Yogur de Prueba 2' }
+        ]
+    });
+};
+
+module.exports = { getTest };
